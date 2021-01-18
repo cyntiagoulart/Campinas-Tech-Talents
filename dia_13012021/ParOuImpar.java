f85c8ee3d40136bb5476addbaf0d48e958682c90
cyntiagoulart <cyntiartg@gmail.com>
2021-01-18 10:22:11 -0300

    Exercícios feitos em aula.

---
+++ dia_13012021/ParOuImpar.java
@@ -0,0 +1,24 @@
+package dia_13012021;
+
+import java.util.Scanner;
+
+/*Escreva um algoritmo que leia um número inteiro e diga se ele é par ou ímpar.*/
+
+public class ParOuImpar {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite um número inteiro: ");
+		int num = scanner.nextInt();
+
+		if (num % 2 == 0) {
+			System.out.println(num + " é par!");
+		} else {
+			System.out.println(num + " é ímpar");
+		}
+		scanner.close();
+	}
+
+}
